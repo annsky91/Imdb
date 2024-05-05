9d723b32ef5c6ee91d93011642fdd7f17722dbc5
Анна Севрюкова <a.sevrukova@getic.com>
2024-05-05 13:26:30 +0300

    Fixes

--- src/App.js
+++ src/App.js
@@ -5,68 +5,66 @@ import RangeInput, { useRangeInput } from './components/RangeInput';
 
 const apiKey = 'k_2pw3cpr8';
 
-function App() {
-  const raiting = useRangeInput();
+export default function App() {
+    const raiting = useRangeInput();
 
-  const [result, setResult] = useState();
-  const [wait, setWait] = useState(false);
+    const [result, setResult] = useState();
+    const [wait, setWait] = useState(false);
 
-  const search = async () => {
-    setWait(true);
-    const params = {};
-    if (raiting.min !== '' && raiting.max !== '') {
-      params.user_rating = raiting.min + ',' + raiting.max;
+    const search = async () => {
+        setWait(true);
+        const params = {};
+        if (raiting.min !== '' && raiting.max !== '') {
+            params.user_rating = raiting.min + ',' + raiting.max;
+        }
+        const res = await axios.get(`https://imdb-api.com/API/AdvancedSearch/${apiKey}`, {
+            params,
+        });
+        setResult(res.data.results);
+        setWait(false);
     }
-    const res = await axios.get(`https://imdb-api.com/API/AdvancedSearch/${apiKey}`, {
-      params,
-    });
-    setResult(res.data.results);
-    setWait(false);
-  }
 
-  return (
-    <div className={styles.main}>
-      <RangeInput title="Рейтинг" {...raiting}/>
-      {wait ? 'Загрузка...' : <button onClick={search}>Поиск</button>}
-      <div className={styles.films}>
-        {result && result.map((film, i) => (
-          <div key={i} className={styles.film}>
-            <h4>{film.title}</h4>
-            <div className={styles.filmImageBlock}>
-              <img className={styles.filmImage} width="200px" src={film.image} alt='Poster'/>
+    return (
+        <div className={styles.main}>
+            <RangeInput title="Рейтинг" {...raiting} />
+            {wait ? 'Загрузка...' : <button onClick={search}>Поиск</button>}
+            <div className={styles.films}>
+                {result && result.map((film, i) => (
+                    <div key={i} className={styles.film}>
+                        <h4>{film.title}</h4>
+                        <div className={styles.filmImageBlock}>
+                            <img className={styles.filmImage} width="200px" src={film.image} alt='Poster' />
+                        </div>
+                        <div>
+                            Content Rating: {film.contentRating}
+                        </div>
+                        <div>
+                            Description: {film.description}
+                        </div>
+                        <div>
+                            Жанры: {film.genres}
+                        </div>
+                        <div>
+                            imDb Rating: {film.imDbRating}
+                        </div>
+                        <div>
+                            imDb Голосов: {film.imDbRatingVotes}
+                        </div>
+                        <div>
+                            Metacritic Rating: {film.metacriticRating}
+                        </div>
+                        <div>
+                            plot: <p>{film.plot}</p>
+                        </div>
+                        <div>
+                            Продолжительность: {film.runtimeStr}
+                        </div>
+                        <div>
+                            Актеры: {film.stars}
+                        </div>
+                    </div>
+                ))}
             </div>
-            <div>
-              Content Rating: {film.contentRating}
-            </div>
-            <div>
-              Description: {film.description}
-            </div>
-            <div>
-              Жанры: {film.genres}
-            </div>
-            <div>
-              imDb Rating: {film.imDbRating}
-            </div>
-            <div>
-              imDb Голосов: {film.imDbRatingVotes}
-            </div>
-            <div>
-              Metacritic Rating: {film.metacriticRating}
-            </div>
-            <div>
-              plot: <p>{film.plot}</p>
-            </div>
-            <div>
-              Продолжительность: {film.runtimeStr}
-            </div>
-            <div>
-              Актеры: {film.stars}
-            </div>
-          </div>
-        ))}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
-
-export default App;
